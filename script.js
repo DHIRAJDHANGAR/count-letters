384a7e4c2142679dae97a10e9f6846abf46679c1
DHIRAJDHANGAR <dhadpe05@gmail.com>
2022-05-26 21:07:43 +0530

    add commends

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
       output = 4A3B2C1D2A2B*/
 
       let data = "AAAABBBCCDAABB";
-      let length = data.length;
+      let length = data.length;   //define lenght of data
       var output = [];
       let count = 1;
       
       for (let i=0; i<length; i++)
       {
-          if(data[i] === data[i+1]){
-              count++;
+          if(data[i] === data[i+1]){    //match first and second letter
+              count++;                  //count how each letter is repeat
           }
-          else{
-              output.push(count + data[i]);
+          else{                         
+              output.push(count + data[i]);  //push count and letter present in data
               count = 1;
           }
       }
